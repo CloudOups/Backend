3eb2538b02fc5f96ecbc82df9b5d10617d0ebf3c
Rania-Ben-Abdallah <74153166+Rania-Ben-Abdallah@users.noreply.github.com>
2024-03-25 12:00:03 +0100

    Revert "crud terrain +equipe"
    This reverts commit d991159c55098347f4b5dab75e91b24279b0dfae.

--- PI/src/main/java/tn/esprit/pi/repositories/IEquipeRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.pi.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import tn.esprit.pi.entities.Equipe;
-
-public interface IEquipeRepository extends CrudRepository <Equipe,Integer>{
-}
--- PI/src/main/java/tn/esprit/pi/repositories/ITerrainRepository.java
+++
@@ -1,7 +0,0 @@
-package tn.esprit.pi.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import tn.esprit.pi.entities.Terrain;
-
-public interface ITerrainRepository extends CrudRepository<Terrain,Integer> {
-}
