f5fabf4ba8a929712af72e03998e58c0e9121adc
islembenaissa <164414766+islembenaissa@users.noreply.github.com>
2024-03-26 00:53:57 +0100

    pull to branch

---
+++ PI/src/main/java/tn/esprit/pi/entities/User.java
@@ -0,0 +1,47 @@
+package tn.esprit.pi.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long userId;
+    String nom;
+    String prenom;
+    String email;
+    String mdp;
+    @Enumerated(EnumType.STRING)
+    Role role;
+    String photo;
+    String classe;
+
+    @OneToMany(mappedBy = "user")
+    Set<ReservationTerrain> reservationTerrains;
+
+    @OneToOne(mappedBy ="user")
+    Ticket ticket;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy ="user")
+    Set<Commentaire> Commentaires;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    Set<Publication> publications;
+
+    @OneToOne(mappedBy = "user")
+    Panier panier;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    Set<Commande> commandes;
+}
