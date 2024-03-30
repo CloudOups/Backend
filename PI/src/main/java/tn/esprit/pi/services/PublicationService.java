3f55be873907650982b1a6268bcf4d06d7893623
hedibousbih <Mohamedhedi.Bousbih@esprit.tn>
2024-03-30 18:27:56 +0100

    calcule monthly pub for statics

--- PI/src/main/java/tn/esprit/pi/services/PublicationService.java
+++ PI/src/main/java/tn/esprit/pi/services/PublicationService.java
@@ -3,11 +3,13 @@ package tn.esprit.pi.services;
 import tn.esprit.pi.entities.Publication;
 import tn.esprit.pi.repositories.IPublicationRepository;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 public class PublicationService implements IPublicationService {
     IPublicationRepository publicationRepository;
-
+    static long totalPublications = 0;
 
     @Override
     public Publication addPublication(Publication publication) {
@@ -51,5 +53,18 @@ public class PublicationService implements IPublicationService {
             return publicationRepository.save(publication);//save publication
         }
         return null;
+    }
+    public void calculeMonthlyPubForStatics() {
+        // Get the current month
+        Month currentMonth = LocalDate.now().getMonth();
+        List<Publication> allPublications = publicationRepository.findAll(); //lehne bech nejbdou publicationet lkol
+        long publicationsPostedThisMonth = allPublications.stream()
+                .filter(publication -> publication.getDateCreation().getMonth() == currentMonth) //nfiltriw publicationet li 3andhom meme mois
+                .count(); //ncomptiw kol publication
+        totalPublications += publicationsPostedThisMonth; //incrementiw nombre total ta3 publication
+
+        // n'affichihom fil console bech na3ref S7a7 ou non
+        System.out.println("Total publications posted this month: " + publicationsPostedThisMonth);
+        System.out.println("Total publications posted overall: " + totalPublications);
     }
 }
